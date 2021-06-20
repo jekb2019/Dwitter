95f251b413df5f7b8d286c55f7e60c4962e22670
jekb2019 <54789857+jekb2019@users.noreply.github.com>
2021-06-21 11:57:35 +1200

    Add Frontend React and Backend mocok data

---
+++ server/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import tweetRouter from './router/tweet.js';
+
+const app = express();
+
+app.use(express.json());
+
+// redirect requests to router
+app.use('/tweets', tweetRouter);
+
+// 테스트용
+app.get('/', (req, res, next) => {
+    console.log('request');
+    res.send('hi');
+});
+
+// final error handling
+app.use((err, req, res, next) => {
+    res.status(500).json({message: "Unexpected server error"});
+});
+
+app.listen(8080);
