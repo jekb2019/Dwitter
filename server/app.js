1764946ea8ee18bb1b3d607a08d07bdd72531d61
jekb2019 <54789857+jekb2019@users.noreply.github.com>
2021-06-21 12:18:07 +1200

    Create template for tweet middlewares

--- server/app.js
+++ server/app.js
@@ -16,6 +16,7 @@ app.get('/', (req, res, next) => {
 
 // final error handling
 app.use((err, req, res, next) => {
+    console.error(err);
     res.status(500).json({message: "Unexpected server error"});
 });
 
--- server/router/tweet.js
+++ server/router/tweet.js
@@ -2,4 +2,64 @@ import express from 'express';
 
 const router = express.Router();
 
+/**
+ * Get all tweets.
+ * Respose format: { [tweet, tweet, ...] }
+ */
+router.get('/', (req, res, next) => {
+    if(!Object.keys(req.query).length !== 0) {
+        next();
+    } else {
+        res.status(404).send("get all tweet")
+    }
+})
+
+/**
+ * Get all tweets for a specified user in "username" query
+ * Response format: { [tweet, tweet, ...] }
+ */
+router.get('/', (req, res, next) => {
+    const query = req.query;
+    res.status(404).send(query);
+})
+
+/**
+ * Get a single tweet for a specified tweet ID
+ * Response format: { tweet }
+ */
+router.get('/:id', (req, res, next) => {
+    const tweetId = req.params.id;
+    res.status(404).send(tweetId);
+})
+
+/**
+ * Create a new tweet
+ * Request format: { text, name, username, url (optional) }
+ * Response format: { tweet }
+ */
+router.post('/', (req, res, next) => {
+    const newTweet = req.body;
+    console.log(newTweet);
+    res.sendStatus(404);
+})
+
+/**
+ * Update a tweet for a specified tweet ID
+ * Request format: { text }
+ * Response format: { tweet }
+ */
+router.put('/:id', (req, res, next) => {
+    const tweetId = req.params.id;
+    const updatedText = req.body;
+    res.sendStatus(404);
+})
+
+/**
+ * Delete a tweet with a specified tweet ID
+ */
+router.delete('/:id', (req, res, next) => {
+    const tweetId = req.params.id;
+    res.sendStatus(404);
+})
+
 export default router;
