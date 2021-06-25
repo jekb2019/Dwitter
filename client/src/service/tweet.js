34a7e8ba1165364201e0d8886e329975860db399
jekb2019 <54789857+jekb2019@users.noreply.github.com>
2021-06-25 14:24:59 +1200

    Refactor http communication to network module

--- client/src/service/tweet.js
+++ client/src/service/tweet.js
@@ -1,97 +1,32 @@
 export default class TweetService {
-  constructor(baseURL) {
-    this.baseURL = baseURL;
+  constructor(http) {
+    this.http = http;
   }
 
-
   async getTweets(username) {
-    if(username) {
-      
-      const url = new URL(process.env.REACT_APP_TWEET_URL)
-      url.search = new URLSearchParams({username}).toString();
-
-      const userTweets = fetch(url)
-      .then(
-        response => {
-          if (response.status !== 200) {
-            throw new Error(response.status);
-          }
-          return response.json();
-        }
-      )
-      .then(data => data["filteredTweet"])
-      .catch(console.error)
-
-      return await userTweets;
-
-    } else {
-      const allTweets = fetch(process.env.REACT_APP_TWEET_URL)
-      .then(
-        response => {
-          if (response.status !== 200) {
-            throw new Error(response.status);
-          }
-          return response.json();
-        }
-      )
-      .then(data => data["allTweets"])
-      .catch(console.error);
-
-      return await allTweets;
-    }
+    const query = username ? `?username=${username}` : '';
+    return this.http.fetch(`/tweets${query}`, {
+      method: 'GET',
+    });
   }
 
   async postTweet(text) {
-    const tweet = {
-      id: Date.now(),
-      createdAt: new Date(),
-      name: 'Ellie',
-      username: 'ellie',
-      text,
-    };
-
-    const postedTweet = fetch(process.env.REACT_APP_TWEET_URL, {
-      method: "POST",
-      body: JSON.stringify(tweet),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8"
-      }
-    })
-    .then(response => response.json())
-
-    return await postedTweet;
+    return this.http.fetch(`/tweets`, {
+      method: 'POST',
+      body: JSON.stringify({ text, username: 'ellie', name: 'Ellie' }),
+    });
   }
 
   async deleteTweet(tweetId) {
-    const url = new URL(process.env.REACT_APP_TWEET_URL + tweetId);
-    fetch(url, {
-      method: "DELETE"
+    return this.http.fetch(`/tweets/${tweetId}`, {
+      method: 'DELETE',
     });
   }
 
   async updateTweet(tweetId, text) {
-    //fetch tweet with tweetId
-    const url = new URL(process.env.REACT_APP_TWEET_URL + tweetId);
-    const tweetById = fetch(url)
-    .then(response => response.json());
-
-    if (!tweetById) {
-      throw new Error('tweet not found!');
-    }
-
-    const fetchedTweet = await tweetById;
-    fetchedTweet.text = text;
-
-    // update tweet in the server
-    const updatedTweet = fetch(url, {
-      method: "PUT",
-      body: JSON.stringify({text}),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8"
-      }
-    })
-    .then(response => response.json());
-
-    return await updatedTweet;
+    return this.http.fetch(`/tweets/${tweetId}`, {
+      method: 'PUT',
+      body: JSON.stringify({ text }),
+    });
   }
 }
