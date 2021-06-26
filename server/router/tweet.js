a337592cd4db27089662b301e2ecb58edcf50c99
jekb2019 <54789857+jekb2019@users.noreply.github.com>
2021-06-27 11:34:39 +1200

    Add sanitization and validation

--- server/router/tweet.js
+++ server/router/tweet.js
@@ -1,21 +1,38 @@
 import express from 'express';
 import * as tweetController from '../controller/tweet.js';
 const router = express.Router();
+import { body, param, query } from 'express-validator'
 
 // GET /tweets
 // GET /tweets?username=:username
-router.get('/', tweetController.getTweets);
+// Validation:
+// Sanitization:
+router.get('/', [query("username").trim()],tweetController.getTweets);
 
 // GET /tweets/:id
-router.get('/:id', tweetController.getTweet);
+// Validation: id valid
+// Sanitization: id sanitization
+router.get('/:id',[param('id').trim().notEmpty().withMessage("ID is empty")] , tweetController.getTweet);
 
 // POST /tweeets
-router.post('/', tweetController.createTweet);
+// Validation: body valid
+// Sanitization: body sanitization
+router.post('/', 
+[
+    body("text").notEmpty().withMessage("Tweet empty"), 
+    body("username").trim().isLength({min: 2, max: 10}).withMessage("Username empty"),
+    body("name").trim().notEmpty().withMessage("Name empty")
+], 
+tweetController.createTweet);
 
 // PUT /tweets/:id
-router.put('/:id', tweetController.updateTweet);
+// Validation:
+// Sanitization:
+router.put('/:id', [body("text").notEmpty().withMessage("Tweet empty"), ],tweetController.updateTweet);
 
 // DELETE /tweets/:id
+// Validation
+// Sanitization
 router.delete('/:id', tweetController.deleteTweet);
 
 export default router;
