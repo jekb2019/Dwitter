d30c306cd048b718a8778b27e03612096610f55f
jekb2019 <54789857+jekb2019@users.noreply.github.com>
2021-06-27 16:43:53 +1200

    Refactor tweet validator

--- server/router/tweet.js
+++ server/router/tweet.js
@@ -4,6 +4,10 @@ import { body, param, query, validationResult } from 'express-validator'
 import {validate} from '../middleware/validator.js';
 
 const router = express.Router();
+const validateTweet = [
+    body("text").trim().isLength({ min:3 }).withMessage("Tweet needs to be at least 3 characters"), 
+    validate
+];
 
 // GET /tweets
 // GET /tweets?username=:username
@@ -13,22 +17,10 @@ router.get('/', [query("username").trim()], tweetController.getTweets);
 router.get('/:id',[param('id').trim()], tweetController.getTweet);
 
 // POST /tweeets
-router.post('/', 
-[
-    body("text").trim().isLength({ min:3 }).withMessage("Tweet needs to be at least 3 characters"), 
-    body("username").trim().isLength({min: 2, max: 10}).withMessage("Username empty"),
-    body("name").trim().notEmpty().withMessage("Name empty"),
-    validate
-], 
-tweetController.createTweet);
+router.post('/',validateTweet, tweetController.createTweet);
 
 // PUT /tweets/:id
-router.put('/:id', 
-    [
-        body("text").notEmpty().withMessage("Tweet empty"),
-        validate 
-    ], 
-tweetController.updateTweet);
+router.put('/:id', validateTweet, tweetController.updateTweet);
 
 // DELETE /tweets/:id
 router.delete('/:id', tweetController.deleteTweet);
